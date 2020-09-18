5697150e4b24dd1eba74272e69aa5f69301eb5e7
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-09-18 23:45:07 +0530

    callbacks are added in end of file

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -62,3 +62,28 @@ local strategy = { 'exact', 'substring', 'fuzzy' }
 vim.g.completion_matching_strategy_list = strategy
 vim.g.diagnostic_enable_virtual_text = 1
 vim.g.space_before_virtual_text = 5
+
+vim.lsp.callbacks['textDocument/codeAction'] =
+require'lsputil.codeAction'.code_action_handler
+
+vim.lsp.callbacks['textDocument/references'] =
+require'lsputil.locations'.references_handler
+
+vim.lsp.callbacks['textDocument/definition'] =
+require'lsputil.locations'.definition_handler
+
+vim.lsp.callbacks['textDocument/declaration'] =
+require'lsputil.locations'.declaration_handler
+
+vim.lsp.callbacks['textDocument/typeDefinition'] =
+require'lsputil.locations'.typeDefinition_handler
+
+vim.lsp.callbacks['textDocument/implementation'] =
+require'lsputil.locations'.implementation_handler
+
+vim.lsp.callbacks['textDocument/documentSymbol'] =
+require'lsputil.symbols'.document_handler
+
+vim.lsp.callbacks['workspace/symbol'] =
+require'lsputil.symbols'.workspace_handler
+
