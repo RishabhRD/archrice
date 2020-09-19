08488c37e45fd18a6772f30656675bdd99490455
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-09-19 14:14:41 +0530

    added some more keybinds

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -9,6 +9,7 @@ local on_attach_common = function(client)
 	require'completion'.on_attach(client)
 	require'diagnostic'.on_attach(client)
 
+	-- GOTO mappings
 	map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
 	map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
 	map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
@@ -17,14 +18,22 @@ local on_attach_common = function(client)
 	map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
 	map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
 	map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
-	map('n','<leader>as','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+	map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+
+	-- ACTION mappings
+	map('n','<leader>ah',  '<cmd>lua vim.lsp.buf.hover()<CR>')
 	map('n','<leader>af', '<cmd>lua vim.lsp.buf.code_action()<CR>')
 	map('n','<leader>ee', '<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
+	map('n','<leader>ar',  '<cmd>lua vim.lsp.buf.rename()<CR>')
+
+	-- Few language severs support these three
+	map('n','<leader>=',  '<cmd>lua vim.lsp.buf.formatting()<CR>')
+	map('n','<leader>ai',  '<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
+	map('n','<leader>ao',  '<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
+
 	-- if diagnostic plugin is installed
-	map('n','<leader>ep','<cmd>PrevDiagnostic<CR>')
-	map('n','<leader>en','<cmd>NextDiagnostic<CR>')
-	map('n','<leader>eP','<cmd>PrevDiagnosticCycle<CR>')
-	map('n','<leader>eN','<cmd>NextDiagnosticCycle<CR>')
+	map('n','<leader>ep','<cmd>PrevDiagnosticCycle<CR>')
+	map('n','<leader>en','<cmd>NextDiagnosticCycle<CR>')
 end
 
 local custom_attach = function(client)
@@ -41,7 +50,7 @@ lsp.sumneko_lua.setup{
 			runtime = { version = "LuaJIT", path = vim.split(package.path, ';'), },
 			completion = { keywordSnippet = "Disable", },
 			diagnostics = { enable = true, globals = {
-				"vim", "describe", "it", "before_each", "after_each","vim.lsp.buf" },
+				"vim", "describe", "it", "before_each", "after_each" },
 			},
 			workspace = {
 				library = {
