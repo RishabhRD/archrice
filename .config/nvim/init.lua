721efcaaecb555edb26195adac85979acbd90e03
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-12-24 19:44:22 +0530

    vim.o doesn't work well all the time

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,38 +1,72 @@
 vim.g.mapleader = ' '
-local  o   =  vim.o
+-- local  o   =  vim.o
+local function set(key, value)
+    if value == true or value == nil then
+	vim.cmd(string.format('set %s', key))
+    elseif value == false then
+	vim.cmd(string.format('set no%s', key))
+    else
+	vim.cmd(string.format('set %s=%s', key, value))
+    end
+end
 
 -- Necessary to work
-o.hidden          =  true
-o.wrap            =  true
-o.autoread        =  true
-o.undofile        =  true
-o.swapfile        =  false -- They disturb too much. But I also want them
-o.backup          =  false
-o.completeopt     =  'menuone,longest,noselect,noinsert'
+-- o.hidden          =  true
+-- o.wrap            =  true
+-- o.autoread        =  true
+-- o.undofile        =  true
+-- o.swapfile        =  false -- They disturb too much. But I also want them
+-- o.backup          =  false
+-- o.completeopt     =  'menuone,longest,noselect,noinsert'
+set 'hidden'
+set 'wrap'
+set 'autoread'
+set 'undofile'
+set ('swapfile', false)
+set ('backup', false)
+set ('completeopt', 'menuone,longest,noselect,noinsert')
 
 -- Tabs configuration
-o.softtabstop     =  4
-o.shiftwidth      =  4
-o.autoindent      =  true
-o.cindent         =  true
+-- o.softtabstop     =  4
+-- o.shiftwidth      =  4
+-- o.autoindent      =  true
+-- o.cindent         =  true
+set ('softtabstop', 4)
+set ('shiftwidth', 4)
+set 'autoindent'
+set 'cindent'
 
 -- Search configuration
-o.hlsearch        =  false
-o.ignorecase      =  true
-o.smartcase       =  true
-o.inccommand      =  'split'
-o.incsearch       =  true
+-- o.hlsearch        =  false
+-- o.ignorecase      =  true
+-- o.smartcase       =  true
+-- o.inccommand      =  'split'
+-- o.incsearch       =  true
+set ('hlsearch', false)
+set 'ignorecase'
+set 'smartcase'
+set ('inccommand', 'split')
+set 'incsearch'
 
 -- Looks and feels
-o.cmdheight       =  1
-o.number          =  true
-o.relativenumber  =  true
-o.splitright      =  true
-o.splitbelow      =  true
-o.updatetime      =  1000
-o.showcmd         =  true
-o.showmode        =  false
-o.colorcolumn     =  '80'
+-- o.cmdheight       =  1
+-- o.number          =  true
+-- o.relativenumber  =  true
+-- o.splitright      =  true
+-- o.splitbelow      =  true
+-- o.updatetime      =  1000
+-- o.showcmd         =  true
+-- o.showmode        =  false
+-- o.colorcolumn     =  '80'
+set ('cmdheight', 1)
+set 'number'
+set 'relativenumber'
+set 'splitright'
+set 'splitbelow'
+set ('updatetime', '1000')
+set 'showcmd'
+set ('showmode', false)
+set ('colorcolumn', '80')
 
 
 -- Text behaviour
@@ -46,8 +80,10 @@ o.colorcolumn     =  '80'
 --                    + 'n'    -- autoformat numbered list
 --                    - '2'    -- I am a programmer and not a writer
 --                    + 'j'    -- Join comments smartly
-o.formatoptions = o.formatoptions..'tcrqnj'
-o.joinspaces = false
+-- o.formatoptions = o.formatoptions..'tcrqnj'
+-- o.joinspaces = false
+set ('formatoptions', 'tcrqnj')
+set ('joinspaces', false)
 
 
 vim.g.text_flavor = "latex"
