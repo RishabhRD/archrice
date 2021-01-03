475e63c037103a4f182c59a80efb51e97997b322
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-01-04 00:39:54 +0530

    formatoptions added in vim style

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,13 +11,6 @@ local function set(key, value)
 end
 
 -- Necessary to work
--- o.hidden          =  true
--- o.wrap            =  true
--- o.autoread        =  true
--- o.undofile        =  true
--- o.swapfile        =  false -- They disturb too much. But I also want them
--- o.backup          =  false
--- o.completeopt     =  'menuone,longest,noselect,noinsert'
 set 'hidden'
 set 'wrap'
 set 'autoread'
@@ -27,21 +20,12 @@ set ('backup', false)
 set ('completeopt', 'menuone,longest,noselect,noinsert')
 
 -- Tabs configuration
--- o.softtabstop     =  4
--- o.shiftwidth      =  4
--- o.autoindent      =  true
--- o.cindent         =  true
 set ('softtabstop', 4)
 set ('shiftwidth', 4)
 set 'autoindent'
 set 'cindent'
 
 -- Search configuration
--- o.hlsearch        =  false
--- o.ignorecase      =  true
--- o.smartcase       =  true
--- o.inccommand      =  'split'
--- o.incsearch       =  true
 set ('hlsearch', false)
 set 'ignorecase'
 set 'smartcase'
@@ -49,15 +33,6 @@ set ('inccommand', 'split')
 set 'incsearch'
 
 -- Looks and feels
--- o.cmdheight       =  1
--- o.number          =  true
--- o.relativenumber  =  true
--- o.splitright      =  true
--- o.splitbelow      =  true
--- o.updatetime      =  1000
--- o.showcmd         =  true
--- o.showmode        =  false
--- o.colorcolumn     =  '80'
 set ('cmdheight', 1)
 set 'number'
 set 'relativenumber'
@@ -68,20 +43,19 @@ set 'showcmd'
 set ('showmode', false)
 set ('colorcolumn', '80')
 
+local fo = require'table_opts':new('formatoptions')
+fo = fo
+   + 't'    -- auto-wrap text using textwidth
+   + 'c'    -- auto-wrap comments using textwidth
+   + 'r'    -- auto insert comment leader on pressing enter
+   - 'o'    -- don't insert comment leader on pressing o
+   + 'q'    -- format comments with gq
+   - 'a'    -- don't autoformat the paragraphs (use some formatter instead)
+   + 'n'    -- autoformat numbered list
+   - '2'    -- I am a programmer and not a writer
+   + 'j'    -- Join comments smartly
+set ('joinspaces', false)
 
--- Text behaviour (think more on this)
--- o.formatoptions = o.formatoptions
---                    + 't'    -- auto-wrap text using textwidth
---                    + 'c'    -- auto-wrap comments using textwidth
---                    + 'r'    -- auto insert comment leader on pressing enter
---                    - 'o'    -- don't insert comment leader on pressing o
---                    + 'q'    -- format comments with gq
---                    - 'a'    -- don't autoformat the paragraphs (use some formatter instead)
---                    + 'n'    -- autoformat numbered list
---                    - '2'    -- I am a programmer and not a writer
---                    + 'j'    -- Join comments smartly
--- o.formatoptions = o.formatoptions..'tcrqnj'
--- o.joinspaces = false
 
 
 vim.g.text_flavor = "latex"
@@ -104,4 +78,3 @@ require'ts_config'
 require'statusline'
 require'colorscheme_config'
 require'finder_config'
-require('telescope').load_extension('fzy_native')
